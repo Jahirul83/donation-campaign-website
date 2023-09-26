4a1be05733e47b9aace6e9fc04be293e85cdd7c8
Jahirul83 <jahirulislamcse83@gmail.com>
2023-09-27 02:14:31 +0600

    coloring the card and make some changes in json files.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,14 +7,13 @@ const Cards = ({ card }) => {
     return (
         <div>
             <Link to={`/details/${id}`}>
-                <div className={`card card-compact shadow-xl ${card_bg_color}`}>
+                <div style={{backgroundColor: card_bg_color}} className="card card-compact shadow-xl">
                     <figure><img src={picture} alt="Shoes" /></figure>
                     <div className="card-body">
                         <div className="card-actions">
-                            <button className={`btn btn-primary`}>{category}</button>
+                            <button style={{color:text_button_bg_color, border:"2px solid", backgroundColor:category_bg_color}} className="btn">{category}</button>
                         </div>
-                        <h2 className={`card-title ${text_button_bg_color}`}>{title}</h2>
-                        {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
+                        <h2 style={{color: text_button_bg_color}} className="card-title">{title}</h2>
                     </div>
                 </div>
             </Link>
